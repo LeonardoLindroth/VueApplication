71aadf905faae35f277074af446540d0a4059c63
LeonardoLindroth <leolindroth14@gmail.com>
2023-03-09 17:31:39 -0500

    saving api and doing some basic variables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+const count = ref(0)
+const title = ref("Home App")
+
+function increment() {
+    count.value++
+}
+
+</script>
+
 <template>
-    <h1>Home App</h1>
+    <h1 @click=increment>{{ title }} ({{ count }})</h1>
 </template>
+
+<style scoped>
+
+</style>
